77964f65bc06dbb1fe0138c33bdb87083b7fb0c2
Tejeswar Das <tejeswar@gmail.com>
2013-10-01 22:33:20 -0700

    minor fix

--- aspenmq/src/main/scala/net/aspenmq/transport/protocol/VariableHeaderEncoder.scala
+++ aspenmq/src/main/scala/net/aspenmq/transport/protocol/VariableHeaderEncoder.scala
@@ -5,10 +5,10 @@ import net.aspenmq.transport.frame.SFrameHeader
 
 trait VariableHeaderEncoder {
   protected def encode(frameHeader: SFrameHeader, protocolBody: ByteBuf): ByteBuf = {
-    val headerBuf2 = new Array[Byte](SFrameHeader.FIXED_HEADER_MAX_LENGTH)
-    val headerBuf = Array.fill[Byte](SFrameHeader.FIXED_HEADER_MAX_LENGTH)(0)
+    val headerBuf = new Array[Byte](SFrameHeader.FIXED_HEADER_MAX_LENGTH)
     val headerLength = frameHeader.marshalHeader(headerBuf)
     val protocolBuf = Unpooled.buffer(headerLength + protocolBody.readableBytes())
+
     protocolBuf.writeBytes(headerBuf, 0, headerLength)
     protocolBuf.writeBytes(protocolBody, protocolBody.readableBytes())
     protocolBuf
